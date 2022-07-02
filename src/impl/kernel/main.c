11e619d306a192bfb5027ce5f19a763279652b97
yuzhe <zhilinlicn@gmail.com>
2022-07-02 20:13:31 +0200

    part2Final

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -2,12 +2,12 @@
  * @Author: yuzhe zhilinlicn@gmail.com
  * @Date: 2022-07-02 18:26:45
  * @LastEditors: yuzhe zhilinlicn@gmail.com
- * @LastEditTime: 2022-07-02 20:00:43
+ * @LastEditTime: 2022-07-02 20:11:26
  * @FilePath: /OS/src/impl/kernel/main.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
-#include"intf/print.h"
+#include"print.h"
 
 void kernel_main() {
     print_clear();
